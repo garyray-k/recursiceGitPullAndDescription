4e8d6c056c6119cd899df42fa3303506bc27cfcb
root <root@gary-alienware.localdomain>
2019-09-05 12:17:18 -0400

    no remote and https added

--- RecursiveGitPullAndDescription.sh
+++ RecursiveGitPullAndDescription.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 # Download this file to /usr/local/bin/git-pull-recursive, then chmod 755 it to install RecursiveGitPullAndDescription
+getDescruption() {
+    # pass the url to this function
+    github=`curl -s $1 | grep description`
+    if [[ $github == "" ]]; then
+        echo "NO DESCRIPTION AVAILABLE."
+        echo "NO DESCRIPTION AVAILABLE." >> ../Descriptions.txt
+        return
+    fi
+    echo "${github:18:((${#github}-2))}"
+    echo "${github:18:((${#github}-2))}" >> ../Descriptions.txt
+}
+
 originalDirectory=$(pwd)
 echo $originalDirectory
-touch Descriptions.txt
+touch Descriptions.txt # find a way to rewrite the descriptions without deleting old ones.
 for repo in $(find . -type d -name .git); do 
     cd $repo/../
     dir=${pwd}
@@ -15,14 +27,17 @@ for repo in $(find . -type d -name .git); do
         endOfUrlWithDotGit=${remoteUrl:15}
         url="https://api.github.com/repos/${remoteUrl:15:((${#endOfUrlWithDotGit} - 4))}"
         echo $url >> ../Descriptions.txt
-        github=`curl -s $url | grep description`
-        echo "${github:18:((${#github}-2))}"
-        echo "${github:18:((${#github}-2))}" >> ../Descriptions.txt
-    elif [[ ${remoteUrl:0:18} == "https://api.github.com/repos/" ]]; then
+        getDescruption $url
+    elif [[ ${remoteUrl:0:14} == "https://github" ]]; then
         echo "https"
+        url="https://api.github.com/repos${remoteUrl:18:((${#endOfUrlWithDotGit} - 4))}"
+        echo $url
+        echo $url >> ../Descriptions.txt
+        getDescruption $url
     else
         echo "Remote source unsupported."
-        continue
+        echo "Remote source unsupported." >> ../Descriptions.txt
+        echo $remoteUrl >> ../Descriptions.txt
     fi
     echo "" >> ../Descriptions.txt
     cd $originalDirectory
